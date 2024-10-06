3db5848040c08eabf89bc7d0cb400f4c4246fcf8
Emil Nenkov <enenkov1337@gmail.com>
2024-10-06 22:17:15 +0300

    Update triangle_area_perimeter.cpp

--- triangle_area_perimeter.cpp
+++ triangle_area_perimeter.cpp
@@ -2,22 +2,13 @@
 
 int main()
 {
-    float side1;
-    float side2;
-    float side3;
-    float result_perimeter;
-    float result_area;
-    float semi_perimeter;
+    double a, b, c;
+    std::cin >> a >> b >> c;
 
-    std::cin >> side1;
-    std::cin >> side2;
-    std::cin >> side3;
+    double result_perimeter = a + b + c;
+    double semi_perimeter = result_perimeter / 2;
+    double result_area = sqrt(semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c));
 
-    result_perimeter = side1 + side2 + side3;
-    semi_perimeter = result_perimeter / 2;
-    result_area = sqrt(semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter - side3));
-
-    std::cout << "Area: " << result_area << '\n';
+    std::cout << "Area: " << result_area << std::endl;
     std::cout << "Perimeter: " << result_perimeter;
 }
-
