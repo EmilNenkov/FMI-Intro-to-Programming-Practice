a97c8f5017402ba2a9fcf65b465b81a597425307
Emil Nenkov <enenkov1337@gmail.com>
2024-10-11 22:04:48 +0300

    Update Task05.cpp

--- Homework_02/Task05.cpp
+++ Homework_02/Task05.cpp
@@ -4,6 +4,8 @@ int main()
 {
     int num;
     std::cin >> num;
+    
     bool is_palindrome = (num % 10 == num / 1000) && (num % 100 / 10 == num / 100 % 10);
+    
     std::cout << is_palindrome;
 }
