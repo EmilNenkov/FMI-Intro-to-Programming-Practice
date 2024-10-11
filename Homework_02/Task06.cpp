3a204cfee17413d5814221ad42841b9970bdeaf7
Emil Nenkov <enenkov1337@gmail.com>
2024-10-11 21:53:49 +0300

    Create Task06.cpp

---
+++ Homework_02/Task06.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int num_burger, num_fish, num_chicken;
+    std::cin >> num_burger >> num_fish >> num_chicken;
+    
+    double sum_order = num_burger * 8.25 + num_fish * 12.00 + num_chicken * 10.99;
+    bool dessert = (sum_order > 60.00);
+    
+    sum_order += dessert * sum_order * 0.1;
+    sum_order += 5.00;
+    
+    std::cout << sum_order;
+}
+
