9c8c72f0bbe192cd397688e5aba9d13c3e50dc20
Emil Nenkov <enenkov1337@gmail.com>
2024-10-16 17:15:54 +0300

    Create Task03.cpp

---
+++ Homework_03/Task03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    char symbol;
+    std::cin >> symbol;
+
+    if ('`' < symbol && symbol < '{') {
+        std::cout << "lowercase letter";
+    }
+    else if ('@' < symbol && symbol < '[') {
+        std::cout << "uppercase letter";
+    }
+    else if ('/' < symbol && symbol < ':') {
+        std::cout << "digit";
+    }
+    else {
+        std::cout << "other";
+    }
+}
+
+
