24baeca5cad820b65232e07e558ce69f07c19e66
Emil Nenkov <enenkov1337@gmail.com>
2024-10-16 16:37:07 +0300

    Create Task01.cpp

---
+++ Homework_03/Task01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int num;
+    std::cin >> num;
+    if (num % 2 == 0) {
+        std::cout << "Even";
+    }
+    else {
+        std::cout << "Odd";
+    }
+}
+
+
