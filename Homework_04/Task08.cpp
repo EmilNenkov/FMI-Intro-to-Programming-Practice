17ff12e78aecb439f36f5473929d73c3ee613ce2
Emil Nenkov <enenkov1337@gmail.com>
2024-10-29 21:43:24 +0200

    Update Task08.cpp

--- Homework_04/Task08.cpp
+++ Homework_04/Task08.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 int main() {
-    int n;
+    int n, lowest_num, highest_num, second_highest_num;
     std::cin >> n;
-    int lowest_num = -2147483648;
-    int highest_num = lowest_num;
-    int second_highest_num = lowest_num;
+    lowest_num = -2147483648;
+    highest_num = second_highest_num = lowest_num;
     
     for (int i = 0; i < n; i++) {
         int num;
