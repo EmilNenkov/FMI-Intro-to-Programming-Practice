66bf0a54873f2139909f650e6057dd5d2782a82f
Emil Nenkov <enenkov1337@gmail.com>
2024-11-06 02:56:05 +0200

    Update Task05.cpp

--- Homework_05/Task05.cpp
+++ Homework_05/Task05.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
-int num_conc(int num1, int num2) {
+int numConc(int num1, int num2) {
     int temp = num2;
     do {
         num1 *= 10;
         temp /= 10;
-    }
-    while (temp);
+    } while (temp);
     return num1 + num2;
 }
 
 int main() {
     int num1, num2;
     std::cin >> num1 >> num2;
-    std::cout << num_conc(num1, num2);
+    std::cout << numConc(num1, num2);
 }
