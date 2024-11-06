412f48102dec999dff43a5ac149c7fdd8f9af51e
Emil Nenkov <enenkov1337@gmail.com>
2024-11-06 02:52:18 +0200

    Update Task04.cpp

--- Homework_05/Task04.cpp
+++ Homework_05/Task04.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 
-bool is_palindrome(int num) {
-    int counter, divisor1, divisor2;
-    counter = divisor1 = divisor2 = 1;
-    int temp = num / 10;
-    
-    while (temp) {
-        counter++;
-        divisor2 *= 10;
-        temp /= 10;
+int reverseNum(int num) {
+    int reversed = 0;
+
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
     }
-    
-    counter /= 2;
-    
-    for (int i = 0; i < counter; i++) {
-        if ((num / divisor1) % 10 != (num / divisor2) % 10) {
-            return false;
-        }
-        divisor1 *= 10;
-        divisor2 /= 10;
-    }
-    return true;
+
+    return reversed;
+}
+
+bool isPalindrome(int num) {
+    int reversed = reverseNum(num);
+    return num == reversed;
 }
 
 int main() {
     int num;
     std::cin >> num;
-    std::cout << std::boolalpha << is_palindrome(num);
+    std::cout << std::boolalpha << isPalindrome(num);
 }
