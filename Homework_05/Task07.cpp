445b75d712c7964f51f804cca27144c3198454d9
Emil Nenkov <enenkov1337@gmail.com>
2024-11-06 03:08:20 +0200

    Update Task07.cpp

--- Homework_05/Task07.cpp
+++ Homework_05/Task07.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-bool desc_ord(int num) {
+bool descOrd(int num) {
     int curr = num % 10;
     while (num / 10) {
         num /= 10;
@@ -12,7 +12,7 @@ bool desc_ord(int num) {
     return true;
 }
 
-bool asc_ord(int num) {
+bool ascOrd(int num) {
     int curr = num % 10;
     while (num) {
         num /= 10;
@@ -20,16 +20,16 @@ bool asc_ord(int num) {
             return false;
         }
         curr = num % 10;
-    } 
+    }
     return true;
 }
 
-bool get_res(int num) {
-    return asc_ord(num) || desc_ord(num);
+bool getRes(int num) {
+    return ascOrd(num) || descOrd(num);
 }
 
 int main() {
     int num;
     std::cin >> num;
-    std::cout << std::boolalpha << get_res(num);
+    std::cout << std::boolalpha << getRes(num);
 }
