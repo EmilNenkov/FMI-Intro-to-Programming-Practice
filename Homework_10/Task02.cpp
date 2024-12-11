77c73f27ac5a27b120b7bb6f1073dd23c99a55c7
Emil Nenkov <enenkov1337@gmail.com>
2024-12-11 14:03:43 +0200

    Create Task02.cpp

---
+++ Homework_10/Task02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+const int SIZE = 2024;
+
+void copyString(const char str1[], char str2[]);
+
+int main()
+{
+    char str1[] = "test";
+    char str2[SIZE];
+    copyString(str1, str2);
+}
+
+void copyString(const char str1[], char str2[]) {
+    while (*str1) {
+        *str2 = *str1;
+        str1++;
+        str2++;
+    }
+}
+
+
+
